5d39829bb9bef35379a1a3ec2f4010d473d5b6dd
kdh <kdh462601@gmail.com>
2024-10-25 18:01:33 +0900

    2024_10_22 과제

---
+++ ASSIGNMENT/JavaScript과제/2024_10_22/객체와 배열_3.js
@@ -0,0 +1,25 @@
+// 배열에 저장된 객체를 순회하는 반복문을 작성한다.
+// 키 `todoId` 의 값이 `2` 인 객체를 찾아서 출력하는 조건문을 작성한다.
+
+let todos = [];
+
+let arrdate = [
+  {
+    todoId: 1,
+    content: '예습하기',
+    isCompleted: false,
+  },
+  {
+    todoId: 2,
+    content: '강의듣기',
+    isCompleted: false,
+  },
+  {
+    todoId: 3,
+    content: '복습하기',
+    isCompleted: false,
+  },
+];
+
+for (index in todos) {
+}
