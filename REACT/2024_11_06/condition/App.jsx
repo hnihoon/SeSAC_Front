f0127ad777eb96bd1fb0c452ebd351f35ac49f10
kdh <kdh462601@gmail.com>
2024-11-06 13:51:50 +0900

    2024_11_06 조건부 렌더링 실습

---
+++ REACT/2024_11_06/condition/App.jsx
@@ -0,0 +1,19 @@
+import { result } from "lodash";
+import Cold from "./Cold";
+import Cold1 from "./Cold1";
+import Cold2 from "./Cold2";
+import Cold3 from "./Cold3";
+import PackingList from "./PackingList";
+function App() {
+  return (
+    <>
+      <PackingList></PackingList>
+      {/* <Cold0></Cold0> */}
+      {/* <Cold1></Cold1> */}
+      {/* <Cold2></Cold2> */}
+      {/* <Cold3></Cold3> */}
+    </>
+  );
+}
+
+export default App;
