7ea25cb0aac3761feec8f129697eb04ed4dfce97
kdh <kdh462601@gmail.com>
2024-12-01 14:32:27 +0900

    불필요한 코드 제거

--- JAVASCRIPT/2024_10_24/callback_function/filter.js
+++ JAVASCRIPT/2024_10_24/callback_function/filter.js
@@ -2,28 +2,28 @@
 
 const numbers = [1, 2, 3, 4];
 
-// const evenNumbers = numbers.filter((num) => {
-//   return num % 2 === 0;
-// });
-
-// console.log(evenNumbers);
-
-// filter를 가지고 소수만 모아보기
-const filter = numbers.filter((num) => {
-  let isPrime = true;
-  if (num == 1) {
-    console.log(123);
-    isPrime = false;
-  }
-  for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
-      isPrime = false;
-    }
-  }
-  return isPrime;
+const evenNumbers = numbers.filter((num) => {
+  return num % 2 === 0;
 });
 
-console.log(filter);
+console.log(evenNumbers);
+
+// filter를 가지고 소수만 모아보기
+// const filter = numbers.filter((num) => {
+//   let isPrime = true;
+//   if (num == 1) {
+//     console.log(123);
+//     isPrime = false;
+//   }
+//   for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       isPrime = false;
+//     }
+//   }
+//   return isPrime;
+// });
+
+// console.log(filter);
 
 // function checkPrime(num) {
 //   let isPrime = true;
--- JAVASCRIPT/2024_10_28/tmdb/tmdb_basic.js
+++ JAVASCRIPT/2024_10_28/tmdb/tmdb_basic.js
@@ -2,13 +2,43 @@ import dotenv from 'dotenv';
 // const dotenv = require('dotenv')
 dotenv.config();
 
+// async function getPost() {
+//   try{
+
+//   const baseURL = 'https://api.themoviedb.org/3/movie'
+//   // const baseURL = 'https://api.themoviedb.org/3'
+//   const apiKey = process.env.TMDB_API_KEY;
+//   // const path = '/popular'
+//   const path = '/top_rated'
+//   const params = new URLSearchParams({
+//     api_key: apiKey,
+//     language: 'ko',
+//   })
+
+//   const URL = `${baseURL}${path}?${params}`;
+//   const response = await fetch(URL);
+//   const data = await response.json();
+  
+//   // const movies = data.results
+//   const movies = data.genre
+//   const movie = movies[0];
+//   // console.log(movie.title);
+//   console.log(movies);
+  
+//    } catch(error){
+//   console.log('Error:', error);
+//   }
+// }
+// getPost();
+
 async function getPost() {
   try{
 
-  const baseURL = 'https://api.themoviedb.org/3/movie'
+  const baseURL = 'https://api.themoviedb.org/3'
+  // const baseURL = 'https://api.themoviedb.org/3'
   const apiKey = process.env.TMDB_API_KEY;
   // const path = '/popular'
-  const path = '/now_playing'
+  const path = '/genre/movie/list'
   const params = new URLSearchParams({
     api_key: apiKey,
     language: 'ko',
@@ -18,9 +48,12 @@ async function getPost() {
   const response = await fetch(URL);
   const data = await response.json();
   
-  const movies = data.results
+  // const movies = data.results
+  const movies = data.genres
   const movie = movies[0];
-  console.log(movie.title);
+  // console.log(movie.title);
+  console.log(movies);
+  
    } catch(error){
   console.log('Error:', error);
   }
